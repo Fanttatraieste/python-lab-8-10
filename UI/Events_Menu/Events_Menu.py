07908d304940960c3b9ca76d2f84ab2b88e2b577
Fanttatraieste <vladuta61@gmail.com>
2023-12-01 18:44:11 +0200

    UI started

---
+++ UI/Events_Menu/Events_Menu.py
@@ -0,0 +1,12 @@
+def ui_add_menu():
+    print("")
+    print("-------- Events Menu --------")
+    print("1. Add new event")
+    print("2. Update event")
+    print("3. Delete Event")
+    print("4. Get Event")
+    print("5. Go back")
+
+    choice = int(input("Your choice: "))
+
+    return choice
